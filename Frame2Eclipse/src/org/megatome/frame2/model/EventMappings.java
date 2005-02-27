e3e9da94dd3c403b3a3f0ce35879901eb80dcb3b
Chad Johnston <cjohnston@megatome.com>
2005-02-27 23:39:20 +0000

    Update copyright date

--- Frame2Eclipse/src/org/megatome/frame2/model/EventMappings.java
+++ Frame2Eclipse/src/org/megatome/frame2/model/EventMappings.java
@@ -3,7 +3,7 @@
  * 
  * Frame2 Open Source License
  * 
- * Copyright (c) 2004 Megatome Technologies. All rights reserved.
+ * Copyright (c) 2004-2005 Megatome Technologies. All rights reserved.
  * 
  * Redistribution and use in source and binary forms, with or without
  * modification, are permitted provided that the following conditions are met:
