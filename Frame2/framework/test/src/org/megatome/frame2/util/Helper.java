3983232d3a32423924bf86efbf4c78cbaecf3bd6
Chad Johnston <cjohnston@megatome.com>
2007-08-28 16:06:53 +0000

    Add method to make events load fully instead of deferring.

--- Frame2/framework/test/src/org/megatome/frame2/util/Helper.java
+++ Frame2/framework/test/src/org/megatome/frame2/util/Helper.java
@@ -65,8 +65,10 @@ import javax.xml.datatype.XMLGregorianCalendar;
 import javax.xml.parsers.DocumentBuilder;
 import javax.xml.parsers.DocumentBuilderFactory;
 
+import org.megatome.frame2.front.TranslationException;
 import org.megatome.frame2.log.Logger;
 import org.megatome.frame2.log.LoggerFactory;
+import org.w3c.dom.Document;
 import org.w3c.dom.Element;
 
 /**
@@ -74,70 +76,106 @@ import org.w3c.dom.Element;
  */
 public class Helper {
 
-    private static Logger LOGGER = LoggerFactory.instance(Helper.class.getName());
+	private static Logger LOGGER = LoggerFactory.instance(Helper.class
+			.getName());
 
-    /**
-     * Returns a string for the calendar with the format '2002-12-25'.
-     * @param cal
-     * @return String
-     */
-    public static String calendarToString(Calendar cal) {
-       Calendar now = Calendar.getInstance();
-       TimeZone tz = now.getTimeZone();
-       int offset = tz.getRawOffset();
+	/**
+	 * Returns a string for the calendar with the format '2002-12-25'.
+	 * 
+	 * @param cal
+	 * @return String
+	 */
+	public static String calendarToString(Calendar cal) {
+		Calendar now = Calendar.getInstance();
+		TimeZone tz = now.getTimeZone();
+		int offset = tz.getRawOffset();
 
-       if (tz.inDaylightTime(now.getTime())) {
-          offset -= tz.getDSTSavings();
-       }
-       
-       if (tz.inDaylightTime(cal.getTime())) {
-          offset += tz.getDSTSavings();
-       }
+		if (tz.inDaylightTime(now.getTime())) {
+			offset -= tz.getDSTSavings();
+		}
 
-       Date d = new Date(cal.getTimeInMillis() - offset);
-       return new SimpleDateFormat("yyyy-MM-dd").format(d); //$NON-NLS-1$
-    }
-    
-    public static String calendarToString(XMLGregorianCalendar cal) {
-    	return calendarToString(cal.toGregorianCalendar());
-     }
+		if (tz.inDaylightTime(cal.getTime())) {
+			offset += tz.getDSTSavings();
+		}
 
-    static public Object unmarshall(String path,String pkg,ClassLoader loader) throws Exception {
-        LOGGER.debug("Unmarshalling " + path + " in package " + pkg); //$NON-NLS-1$ //$NON-NLS-2$
-        JAXBContext jc = JAXBContext.newInstance(pkg, loader);
-        Unmarshaller u = jc.createUnmarshaller();
-		  InputStream istream = ClassLoader.getSystemResourceAsStream(path);
+		Date d = new Date(cal.getTimeInMillis() - offset);
+		return new SimpleDateFormat("yyyy-MM-dd").format(d); //$NON-NLS-1$
+	}
 
-        return u.unmarshal(istream);
-    }
+	public static String calendarToString(XMLGregorianCalendar cal) {
+		return calendarToString(cal.toGregorianCalendar());
+	}
 
-    static public OutputStream marshall(Object obj,String pkg,ClassLoader loader) throws Exception {
-        LOGGER.debug("Marshalling " + obj + " in package " + pkg); //$NON-NLS-1$ //$NON-NLS-2$
-        JAXBContext jc = JAXBContext.newInstance(pkg, loader);
-        Marshaller m = jc.createMarshaller();
-        OutputStream ostream = new ByteArrayOutputStream();   
-      
-        m.marshal(obj, ostream);
+	static public Object unmarshall(String path, String pkg, ClassLoader loader)
+			throws Exception {
+		LOGGER.debug("Unmarshalling " + path + " in package " + pkg); //$NON-NLS-1$ //$NON-NLS-2$
+		JAXBContext jc = JAXBContext.newInstance(pkg, loader);
+		Unmarshaller u = jc.createUnmarshaller();
+		InputStream istream = ClassLoader.getSystemResourceAsStream(path);
 
-        return ostream;
-    }
+		return u.unmarshal(istream);
+	}
 
-	static public InputStream getInputStreamFor(String path,Class<?> clazz) {
-      return clazz.getClassLoader().getResourceAsStream(path);
-   }
+	static public OutputStream marshall(Object obj, String pkg,
+			ClassLoader loader) throws Exception {
+		LOGGER.debug("Marshalling " + obj + " in package " + pkg); //$NON-NLS-1$ //$NON-NLS-2$
+		JAXBContext jc = JAXBContext.newInstance(pkg, loader);
+		Marshaller m = jc.createMarshaller();
+		OutputStream ostream = new ByteArrayOutputStream();
 
-	static public Element[] loadEvents(String path,Class<?> clazz) throws Exception {
-      Element[] result = new Element[1];
-      DocumentBuilder builder = DocumentBuilderFactory.newInstance().newDocumentBuilder();
+		m.marshal(obj, ostream);
 
-      result[0] = builder.parse(Helper.getInputStreamFor(path,clazz)).getDocumentElement();
+		return ostream;
+	}
+	
+	static public Document marshall(Object obj, String pkg, Document targetDoc, ClassLoader loader) throws Exception {
+		JAXBContext jc = JAXBContext.newInstance(pkg, loader);
+		Marshaller m = jc.createMarshaller();
 
-      return result;
-   }
+		m.marshal(obj, targetDoc);
 
+		return targetDoc;
+	}
 
+	static public InputStream getInputStreamFor(String path, Class<?> clazz) {
+		return clazz.getClassLoader().getResourceAsStream(path);
+	}
 
+	static public Element[] loadEvents(String path, String pkg,
+			ClassLoader loader) throws Exception {
+		Object o = unmarshall(path, pkg, loader);
+		Document doc = marshall(o, pkg, getTargetDocument(), loader);
+		return new Element[] {doc.getDocumentElement()};
+	}
 
+	static public Element[] loadEvents(String path, Class<?> clazz)
+			throws Exception {
+		Element[] result = new Element[1];
+		DocumentBuilder builder = DocumentBuilderFactory.newInstance()
+				.newDocumentBuilder();
 
+		result[0] = builder.parse(Helper.getInputStreamFor(path, clazz))
+				.getDocumentElement();
+		result[0].getChildNodes();
+		return result;
+	}
 
+	private static Document getTargetDocument() throws TranslationException {
+		Document result = null;
+
+		try {
+			DocumentBuilderFactory dbf = DocumentBuilderFactory.newInstance();
+
+			dbf.setNamespaceAware(true);
+
+			DocumentBuilder db = dbf.newDocumentBuilder();
+
+			result = db.newDocument();
+		} catch (Exception e) {
+			throw new TranslationException("Unable to create target document", //$NON-NLS-1$
+					e);
+		}
+
+		return result;
+	}
 }
